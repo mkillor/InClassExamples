768d89b84add13a31f770036c168ca280205f136
mak1115 <mak1115@swcsd.us>
2019-01-09 14:18:46 -0500

    added more than hello world lol

--- src/edu/cscc/Main.java
+++ src/edu/cscc/Main.java
@@ -3,7 +3,35 @@ package edu.cscc;
 public class Main {
 
     public static void main(String[] args) {
-	    // Basic Hello World application
-        System.out.println("Hello World!");
+        int currentTemperature = 50;
+        int highTemperature = 68;
+        double currentHumidity = 0.55;
+        double highHumidity = 0.75;
+        String cityName = "Columbus";
+
+        //Compound assignment
+        highHumidity += 0.1; //add to existing value
+        currentHumidity /= 2; //divide existing value
+        System.out.println(highHumidity);
+        System.out.println(currentHumidity);
+
+        //Unary increment and decrement
+        int tomorrowHighTemperature = ++highTemperature; //prefix increment
+        int tomorrowLowTemperature = currentTemperature--; //postfix decrement
+        System.out.println(tomorrowHighTemperature);
+        System.out.println(tomorrowLowTemperature);
+
+        /*
+        Comparison
+        compare two numeric values; if the values are the same, isHigh will be true
+        if the values are different, isHigh will be false
+        */
+
+        boolean isHigh = currentTemperature == highTemperature;
+
+        //compare the content of two strings
+        boolean isColumbus = cityName == "Columbus";
+        System.out.println(isHigh);
+        System.out.println(isColumbus);
     }
 }
